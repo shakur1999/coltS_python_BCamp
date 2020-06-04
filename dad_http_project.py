9a71f639fbb34933dcb61b1997c44d1040b3b644
Shakur Xussein <shakurxussein@Shakurs-MacBook-Pro.local>
2020-06-04 14:11:18 -0700

    pending solution

--- dad_http_project.py
+++ dad_http_project.py
@@ -4,11 +4,15 @@ import random
 # prompt input topic 
 # take that input topic and search the web
 
-input("Let me tell you a joke! Give me a topic: ")
-int(input(f"I've got {user_iput} jokes about {user_input}. Here's one: "))
+user_input = input("Let me tell you a joke! Give me a topic: ")
+# int(input(f"I've got {user_iput} jokes about {user_input}. Here's one: "))
 
 url = ("https://icanhazdadjoke.com/search")
 res = requests.get(url,
-                    headers= {"accepts": "application/json"},
-                    params = {"term": "user_input", "limit": 20}
-)
+                    headers= {"accept": "application/json"},
+                    params = {"term": user_input, "limit": 20}
+).json()
+
+print(res)
+# data = res.json()
+# print(data[results])
